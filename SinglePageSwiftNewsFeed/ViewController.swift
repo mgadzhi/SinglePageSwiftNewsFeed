2f0d6e5fe9027b1f18ccabffc2aaaa23a9237478
endenis <endenis@gmail.com>
2015-04-18 15:26:41 +0200

    Hello world with UITableView

--- SinglePageSwiftNewsFeed/ViewController.swift
+++ SinglePageSwiftNewsFeed/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var tableData: [[String: String]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fetchRemoteData();
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func fetchRemoteData() {
+        self.tableData.append(["title": "Title 1", "author": "Ivan"])
+        self.tableData.append(["title": "Title 2", "author": "Maria"])
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        let dataEntry = self.tableData[indexPath.row]
+        cell.textLabel!.text = dataEntry["title"] as String?
+        cell.detailTextLabel!.text = dataEntry["author"] as String?
+        return cell
+    }
 
 }
 
