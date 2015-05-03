bbf30cb3b1a3d1d512af634f9aa6674db56a9f3e
endenis <endenis@gmail.com>
2015-05-03 17:23:24 +0200

    Empty EntryViewController

---
+++ SinglePageSwiftNewsFeed/EntryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EntryViewController.swift
+//  SinglePageSwiftNewsFeed
+//
+//  Created by Denis Engels on 03/05/15.
+//  Copyright (c) 2015 Gadzhi Musaev. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController : UIViewController {
+}
