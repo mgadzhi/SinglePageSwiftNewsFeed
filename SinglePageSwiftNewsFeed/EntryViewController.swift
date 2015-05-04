1a19ff3e623bc2eba02888fc1c055215d9634052
gadzhi <musaevgadzhi@gmail.com>
2015-05-04 17:33:25 +0300

    First version of webview.

--- SinglePageSwiftNewsFeed/EntryViewController.swift
+++ SinglePageSwiftNewsFeed/EntryViewController.swift
@@ -9,4 +9,18 @@
 import UIKit
 
 class EntryViewController : UIViewController {
+    
+    @IBOutlet var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = NSURL(string: "http://carrick.ru/news/van-gaal-persie-oy-oy/")
+        let req = NSURLRequest(URL: url!)
+        webView.loadRequest(req)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
